97c564727a77bc673d627be2a7f172f4925ba721
WunnaO <wunnaoo1882020@gmail.com>
2023-09-24 08:50:43 +0630

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden pb-5 lg:pb-0">
       <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
         <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
           <LargeHeading
